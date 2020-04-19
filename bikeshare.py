6762406352a1ab7127ac5f211f1d846feb72ed82
Laura <larzuvva@gmail.com>
2020-04-19 12:20:24 +0200

    Added pandas.DataFrame.head to control number of displayed rows

--- bikeshare.py
+++ bikeshare.py
@@ -208,10 +208,10 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        time_stats(df.head())
+        station_stats(df.head())
+        trip_duration_stats(df.head())
+        user_stats(df.head())
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
